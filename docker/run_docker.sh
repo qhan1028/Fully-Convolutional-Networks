6fe875aa6f9a72f23f4e01fb856bc188e8a85074
qhan <michael.841028@gmail.com>
2017-08-22 16:51:52 +0800

    fix Dockerfile bug

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,1 +1,1 @@
-nvidia-docker run --rm -it -v /nas1/qhan/Fully-Convolutional-Networks:/app qhan1028/fcn bash
+nvidia-docker run --rm -it -v /nas1/qhan/Fully-Convolutional-Networks:/app qhan1028/fcn $@
